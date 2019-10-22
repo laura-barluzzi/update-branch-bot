af132ce59e7d6936aa6bd28e92782eb194890306
Clemens Wolff <clewolff@microsoft.com>
2019-10-22 16:03:34 -0400

    Refactor main to class for easier testing via IoC

--- AzWebhookWorker/index.js
+++ AzWebhookWorker/index.js
@@ -1,12 +1,19 @@
 const process = require('process')
-const main = require('../lib/main')
+const Git = require('../lib/git')
+const Github = require('../lib/github')
+const Main = require('../lib/main')
 
 async function worker (context, queueItem) {
-  await main({
-    authToken: process.env.GITHUB_TOKEN,
-    log: context.log,
-    ...queueItem
+  const log = context.log
+  const authToken = process.env.GITHUB_TOKEN
+
+  const main = new Main({
+    git: new Git({ log }),
+    github: new Github({ authToken, log }),
+    log
   })
+
+  await main.run(queueItem)
 
   context.done()
 }
