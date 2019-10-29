f96b9ec6b85f27588e19e325820df9a389497c6e
Laura Barluzzi <laura@getadblock.com>
2019-10-29 16:29:05 -0400

    Start debugging error in GcpWebhookWorker

--- GcpWebhookWorker/index.js
+++ GcpWebhookWorker/index.js
@@ -1,3 +1,8 @@
+const process = require('process')
+const Git = require('../lib/git').Git
+const Github = require('../lib/github')
+const Main = require('../lib/main')
+
 /**
  * @param {object} pubsubMessage
  * @param {string} pubsubMessage.data
@@ -5,4 +10,15 @@
 module.exports = pubsubMessage => {
   const message = Buffer.from(pubsubMessage.data, 'base64').toString();
   const branch = JSON.parse(message)
+  console.log(branch);
+  const log = console.log;
+  const authToken = process.env.GITHUB_TOKEN
+
+  const main = new Main({
+    git: new Git({ log }),
+    github: new Github({ authToken, log }),
+    log
+  })
+
+  main.run(branch)
 };
